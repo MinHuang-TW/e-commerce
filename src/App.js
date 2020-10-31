4cf308c43d8ac067ecf40db414d889bfd52c3d68
Min Huang <h.min719@gmail.com>
2020-10-31 19:53:37 +0100

    [CollectionPreview, CollectionItem] added new components for ShopPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
+import { HomePage, ShopPage } from './pages';
 
 const App = () => (
   <div>
     <Switch>
       <Route path='/' component={HomePage} exact />
+      <Route path='/shop' component={ShopPage} />
     </Switch>
   </div>
 );
--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -1,45 +1,10 @@
 import React, { useState } from 'react';
 import MenuItem from '../MenuItem/MenuItem';
+import SECTION_DATA from './section.data.js';
 import './Directory.styles.scss';
 
-const data = [
-  {
-    id: 1,
-    title: 'hats',
-    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    linkUrl: 'shop/hats',
-  },
-  {
-    id: 2,
-    title: 'jackets',
-    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    linkUrl: 'shop/jackets',
-  },
-  {
-    id: 3,
-    title: 'sneakers',
-    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    linkUrl: 'shop/sneakers',
-  },
-  {
-    id: 4,
-    title: 'womens',
-    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    size: 'large',
-    linkUrl: 'shop/womens',
-  },
-  {
-    id: 5,
-    title: 'mens',
-    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    size: 'large',
-    linkUrl: 'shop/mens',
-  },
-];
-
 const Directory = () => {
-  // eslint-disable-next-line
-  const [sections, useSections] = useState(data);
+  const [sections] = useState(SECTION_DATA);
   return (
     <div className='directory-menu'>
       {sections.map(({ id, ...otherSectionProps }) => (
