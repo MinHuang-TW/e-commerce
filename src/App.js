f4b33fc1bb531eee7b386868f9e5af4a3b47c173
Min Huang <h.min719@gmail.com>
2020-10-31 20:11:03 +0100

    [Header] added logo and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import { HomePage, ShopPage } from './pages';
+import Header from './components/Header/Header';
 
 const App = () => (
   <div>
+    <Header />
     <Switch>
       <Route path='/' component={HomePage} exact />
       <Route path='/shop' component={ShopPage} />
