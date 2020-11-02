370fbb0cf0d9d6625a5951e3e491dc6f084e5c42
Min Huang <h.min719@gmail.com>
2020-11-02 16:46:42 +0100

    [cart-utils, cart-actions, cart-reducer] add AddItem action to CollectionItem

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Button.styles.scss';
-const Button = ({ children, isGoogleSignIn, ...otherProps }) => (
+const Button = ({ children, inverted, isGoogleSignIn, ...otherProps }) => (
   <button
-    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+    className={`${inverted ? 'inverted' : ''} ${
+      isGoogleSignIn ? 'google-sign-in' : ''
+    } custom-button`}
     {...otherProps}
   >
     {children}
