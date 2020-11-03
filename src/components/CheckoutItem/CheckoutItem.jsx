50e070ba8ae2984c62dd370952470e0474b90e4a
Min Huang <h.min719@gmail.com>
2020-11-03 12:21:08 +0100

    [cart-actions, cart-reducer, CheckoutItem] added addItem & removeItem actions to CheckoutItem

--- src/components/CheckoutItem/CheckoutItem.jsx
+++ src/components/CheckoutItem/CheckoutItem.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {
+  addItem,
+  removeItem,
+  clearItemFromCart,
+} from '../../redux/cart/cart-actions';
 import './CheckoutItem.styles.scss';
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
-  <div className='checkout-item'>
-    <div className='image-container'>
-      <img src={imageUrl} alt='item' />
+const CheckoutItem = ({ cartItem, addItem, removeItem, clearItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div className='checkout-item'>
+      <div className='image-container'>
+        <img src={imageUrl} alt='item' />
+      </div>
+      <span className='name'>{name}</span>
+      <span className='quantity'>
+        <div className='sign' onClick={() => removeItem(cartItem)}>
+          &#x2212;
+        </div>
+        <div className='value'>{quantity}</div>
+        <div className='sign' onClick={() => addItem(cartItem)}>
+          &#x002B;
+        </div>
+      </span>
+      <span className='price'>{price}</span>
+      <span className='remove-button' onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </span>
     </div>
-    <span className='name'>{name}</span>
-    <span className='quantity'>{quantity}</span>
-    <span className='price'>{price}</span>
-    <span className='remove-button'>&#10005;</span>
-  </div>
-);
+  );
+};
 
-export default CheckoutItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
