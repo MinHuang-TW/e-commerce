3e5fc0e323608fad089763fffb53b1b9a3baf7b5
Min Huang <h.min719@gmail.com>
2020-11-10 14:56:15 +0100

    removed SW

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
-import * as serviceWorker from './serviceWorker';
+
 import App from './App';
 import './index.css';
 
@@ -18,5 +18,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-serviceWorker.register();
